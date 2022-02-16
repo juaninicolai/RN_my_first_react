c3e2e14c9e98565b03d7174a26de7a667053ce8a
juaninicolai <juan0719@stud.kea.dk>
2022-02-16 10:08:07 +0100

    todo app

--- App.js
+++ App.js
@@ -1,122 +1,19 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    {
-      userId: 1,
-      id: 1,
-      title: "delectus aut autem",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 2,
-      title: "quis ut nam facilis et officia qui",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 3,
-      title: "fugiat veniam minus",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 4,
-      title: "et porro tempora",
-      completed: true,
-    },
-    {
-      userId: 1,
-      id: 5,
-      title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 6,
-      title: "qui ullam ratione quibusdam voluptatem quia omnis",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 7,
-      title: "illo expedita consequatur quia in",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 8,
-      title: "quo adipisci enim quam ut ab",
-      completed: true,
-    },
-    {
-      userId: 1,
-      id: 9,
-      title: "molestiae perspiciatis ipsa",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 10,
-      title: "illo est ratione doloremque quia maiores aut",
-      completed: true,
-    },
-  ]);
-
-  const pressHandler = (id) => {
-    // const selectedItems = todos.filter((todo) => todo.id === id)[0];
-    // alert(selectedItems.title);
-    let returnValues = [...todos];
-    returnValues.forEach((todo) => {
-      if (todo.id === id) {
-        todo.completed = true;
-      }
-    });
-    setTodos(returnValues);
-  };
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={todos}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text
-              style={{
-                ...styles.todo,
-                backgroundColor: item.completed ? "green" : "deeppink",
-              }}
-            >
-              {item.title}
-            </Text>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
-  );
+  <View>
+    <Text>Hello World</Text>
+    <StatusBar style="auto" />
+  </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-  myText: {
-    fontSize: 20,
-  },
-  todo: {
-    margin: 30,
-    color: "white",
-    fontSize: 30,
-    padding: 30,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
